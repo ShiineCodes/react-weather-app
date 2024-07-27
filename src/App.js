7b25d1f1f20f13429afab1b5f86237443d88eb31
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-07-27 23:26:38 +0200

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather App</div>
+    <div className="App"> 
+    <h1>Weather App </h1> 
+    <footer> This project was coded by Edona Rexhaj and is 
+    <a href="https://github.com/ShiineCodes/react-weather-app" target="_blank">
+    open-sourced on GitHub.</a>
+  </footer>
+    </div>
   );
 }
 
