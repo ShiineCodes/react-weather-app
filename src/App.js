c2a11bc6d1e5dfa9b688a75a80db16bc21762792
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-07-27 23:50:32 +0200

    Added bootsrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-
 import './App.css';
-
 
 export default function App() {
   return (
-    <div className="App"> 
-    <h1>Weather App </h1> 
-    <footer> This project was coded by Edona Rexhaj and is 
-    <a href="https://github.com/ShiineCodes/react-weather-app" target="_blank">
-    open-sourced on GitHub.</a>
-  </footer>
+    <div className="App">
+      <div className="container">
+      <h1>Weather App </h1>
+      <footer>
+        {' '}
+        This project was coded by Edona Rexhaj and is{' '}
+        <a
+          href='https://github.com/ShiineCodes/react-weather-app'
+          target='_blank'
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
     </div>
   );
 }
-
