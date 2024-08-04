e4a2811690e3f99f171d705e4498d52b13611cba
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-08-04 22:38:23 +0200

    Build problem to host on Netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import './App.css';
-import Weather from "./Weather";
+import Weather from './Weather';
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-      <Weather defaultCity="stockholm"/>
-      <footer className='footer'>
-        {' '}
-        This project was coded by Edona Rexhaj and is{' '}
-        <a
-          href='https://github.com/ShiineCodes/react-weather-app'
-          target='_blank'
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
-    </div>
+    <div className='App'>
+      <div className='container'>
+        <Weather defaultCity='stockholm' />
+        <footer className='footer'>
+          {' '}
+          This project was coded by Edona Rexhaj and is{' '}
+          <a
+            href='https://github.com/ShiineCodes/react-weather-app'
+            target='_blank'
+            rel='noreferrer'
+          >
+            open-sourced on GitHub
+          </a>{' '}
+          and hosted on{' '}
+          <a
+            href='https://reactweatherapplication2.netlify.app/'
+            target='_blank'
+            rel='noreferrer'
+          >Netlify </a>
+        </footer>
+      </div>
     </div>
   );
 }
