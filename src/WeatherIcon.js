0170462e6261a1a6bdc14bd27c463be4d2621ce9
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-08-09 00:32:27 +0200

    Changed the style , final touches

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,8 +26,8 @@ export default function WeatherIcon(props) {
     return  (
         <ReactAnimatedWeather
         icon={codeMapping[props.code]}
-        color='black'
-        size="55"
+        color='#6f42c1'
+        size={props.size}
         animate={true}
         />
     )
